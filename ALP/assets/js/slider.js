5f393bf7167a0949a98e046d0978d945511d9d68
GeorgeMarakakis <grg.mrkks@gmail.com>
2016-04-26 20:12:21 +0300

    First Release
    This was the version we presented to our client.
    The first payment, came after this!
    
    However, we have spotted some bugs...
    One of these was the static url at the image slider.

---
+++ ALP/assets/js/slider.js
@@ -0,0 +1,232 @@
+var total_street = 24;
+var total_music = 12;
+var total_portrait = 21;
+var total_wallpaper = 15;
+var total = 0;
+var param = 1;
+var cat = null;
+
+var scaledVal = [];
+var mainContent_width;
+var mainContent_height;
+
+$( document ).ready(function() {
+    // First you forcibly request the scroll bars to hidden regardless if they will be needed or not.
+	$('body').css('overflow', 'hidden');
+	
+	var x = $(window).width();	//viewport width
+	var y = $(window).height(); //viewport height
+	
+	// Set the overflow css property back to it's original value (default is auto)
+	$('body').css('overflow', 'auto');
+		
+	mainContent_width = 0.73*x;		
+	mainContent_height = y-100;		//10px margin-bottom(#slider) and 85px margin-top(#maincontent)
+});
+
+function loadFunc(){
+		var element = document.getElementById('mainContainer');
+		element.style.opacity = "1";
+		element.style.filter  = 'alpha(opacity=100)';
+		initializeSlider();	
+}
+	
+function initializeSlider(){
+		var myParam = getURLParameter('img');
+		param = myParam;
+		
+		var myParam1 = getURLParameter('cat');
+		cat = myParam1;
+		
+		var element = document.getElementById('image');
+		element.src = "../../assets/img/portfolio/"+cat+"/"+cat+""+param+".jpg";
+		
+		var tmpImg = new Image();
+		tmpImg.src="../../assets/img/portfolio/"+cat+"/"+cat+""+param+".jpg";
+		$(tmpImg).on('load',function(){
+  	
+			//Calculate image's initial width and height
+			var orgWidth = tmpImg.width;
+  			var orgHeight = tmpImg.height;
+			var windowWidth = $(window).width();
+			
+			scaledVal = calculateDownscale(orgWidth,orgHeight,mainContent_width,mainContent_height,windowWidth);
+			
+			
+			$("#image").css({"width":scaledVal[0]+"px", "height":scaledVal[1]+"px", "margin":"auto", "position":"relative", "display":"block"});			
+			
+		});
+}
+
+function getURLParameter(name) {
+  		name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function photo(x) {
+	
+	//Different total number of images in categories
+	switch (cat) {
+        case "street":
+            total = total_street;
+			break;
+        case "music":
+			total = total_music;
+            break;
+        case "portrait":
+            total = total_portrait;
+			break;
+        case "wallpaper":
+			total = total_wallpaper;
+            break;
+    }
+	
+	var image = document.getElementById('image');
+	param = parseInt(param) + x;
+	if(param > total){param = 1;}
+	if(param < 1){param = total;}
+	image.src = "../../assets/img/portfolio/"+cat+"/"+cat+""+param+".jpg";
+	
+	/*Load the dimensions of the image before loaded in the browser to change the image style*/	
+	var tmpImg = new Image();
+	tmpImg.src="../../assets/img/portfolio/"+cat+"/"+cat+""+param+".jpg"; 
+	$(tmpImg).on('load',function(){
+  		
+		var orgWidth = tmpImg.width;
+  		var orgHeight = tmpImg.height;
+		var windowWidth = $(window).width();
+		
+		scaledVal = calculateDownscale(orgWidth,orgHeight,mainContent_width,mainContent_height,windowWidth);
+			
+			
+		
+		$("#image").css({"width":scaledVal[0]+"px", "height":scaledVal[1]+"px", "margin":"auto", "position":"relative", "display":"block"});			
+					
+	});
+	
+}
+
+
+//Function for downscale calculation of initial image's dimensions	
+function calculateDownscale(orgWidth,orgHeight,mainContent_width,mainContent_height,windowWidth){
+
+	var scaledDim = [];		
+	scaledDim[0] = 0;	//scaled width
+	scaledDim[1] = 0;	//scaled height
+	
+	console.log("Window Width: "+windowWidth);
+
+	console.log("Original Dimensions of image: "+orgWidth+" x "+orgHeight);
+	
+	console.log("Dimensions of maincontent: "+mainContent_width+" x "+mainContent_height);
+
+	//Calculate downscale ratio of width
+	var downscaleRatio = mainContent_width/orgWidth;
+	
+	//screen-width is more than 1190px
+	if (windowWidth>=1190) {
+	
+			//If donwscale ratio >= 1.00 then put the image as it is, in the initial width
+			if(downscaleRatio >= 1){
+				
+				//Check if downscale ratio fits in the maincontent against the height of the div
+				if(downscaleRatio * orgHeight <= mainContent_height){
+					scaledDim[0] = orgWidth;
+					scaledDim[1] = orgHeight;
+				}
+				//Calculate new downscale ratio by image's height
+				else{
+					downscaleRatio = mainContent_height/orgHeight;
+					scaledDim[0] = downscaleRatio * orgWidth;
+					scaledDim[1] = downscaleRatio * orgHeight;
+				}
+				
+			}
+			else{	//Doesn't fit in the width of maincontent
+				
+				downscaleRatio = mainContent_height/orgHeight;
+				scaledDim[0] = downscaleRatio * orgWidth;
+				scaledDim[1] = downscaleRatio * orgHeight;
+				
+			}
+			
+	}
+	else{//screen width is less than 1190px - image gets full width
+			
+			if(downscaleRatio >= 1){
+				scaledDim[0] =  orgWidth;
+				scaledDim[1] =  orgHeight;
+			}
+			else{
+				scaledDim[0] = downscaleRatio * orgWidth;
+				scaledDim[1] = downscaleRatio * orgHeight;
+			}
+	}
+			
+	console.log("Dimensions of scaled image: "+scaledDim[0]+" x "+scaledDim[1]);
+	return scaledDim;
+}	
+	
+	
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            photo(-1);	//Left arrow
+			break;
+        case 38:
+            break;
+        case 39:
+            photo(1);	//Right arrow
+			break;
+        case 40:
+            break;
+		case 27:
+			window.history.back();
+    }
+};
+
+
+/*$(function() {
+
+	// open in fullscreen
+	$('#fullscreen .requestfullscreen').click(function() {
+		$('#fullscreen').fullscreen();
+		return false;
+	});
+
+	// exit fullscreen
+	$('#fullscreen .exitfullscreen').click(function() {
+		$.fullscreen.exit();
+		return false;
+	});
+
+	// document's event
+	$(document).bind('fscreenchange', function(e, state, elem) {
+		// if we currently in fullscreen mode
+		if ($.fullscreen.isFullScreen()) {
+			$('#fullscreen .requestfullscreen').hide();
+			$('#fullscreen .exitfullscreen').show();
+			$('#fullscreen .exitfullscreen').css({"visibility":"visible"});
+		} else {
+			$('#fullscreen .requestfullscreen').show();
+			$('#fullscreen .exitfullscreen').hide();
+			$('#fullscreen .exitfullscreen').css({"visibility":"hidden"});
+		}
+
+		/*$('#state').text($.fullscreen.isFullScreen() ? '' : 'not')
+	});
+});
+
+$('.autoPlay').bind('click', function( event ){
+               alert('Hi there!');
+            });*/
+	
+/*window.setInterval(function photoA() {
+	var image = document.getElementById('image');
+	imageCount = imageCount + 1;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "Images/img"+ imageCount +".jpg";
+	},5000);*/
